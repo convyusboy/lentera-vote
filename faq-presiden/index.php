8ff6e034078a33c5b44b9ee0c00b3b24883aa68b
Ridho Akbarisanto <ridho.akbarisanto@yahoo.com>
2014-06-11 10:26:14 +0700

    Ngedit faq-presiden n laporan_pelanggaran

--- faq-presiden/index.php
+++ faq-presiden/index.php
@@ -1,6 +1,40 @@
 <html>
 <head>
 	<title></title>
+	<style type="text/css">
+	.tooltip {
+		border-bottom: 1px dotted #000000; color: #000000; outline: none;
+		cursor: help; text-decoration: none;
+		position: relative;
+	}
+	.tooltip span {
+		margin-left: -999em;
+		position: absolute;
+	}
+	.tooltip:hover span {
+		border-radius: 5px 5px; -moz-border-radius: 5px; -webkit-border-radius: 5px; 
+		box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1); -webkit-box-shadow: 5px 5px rgba(0, 0, 0, 0.1); -moz-box-shadow: 5px 5px rgba(0, 0, 0, 0.1);
+		font-family: Calibri, Tahoma, Geneva, sans-serif;
+		position: absolute; left: 0; top: 20; z-index: 99;
+		margin-left: 0; width: 1000px;
+	}
+	.tooltip:hover img {
+		border: 0; margin: -10px 0 0 -55px;
+		float: left; position: absolute;
+	}
+	.tooltip:hover em {
+		font-family: Candara, Tahoma, Geneva, sans-serif; font-size: 1.2em; font-weight: bold;
+		display: block; padding: 0.2em 0 0.6em 0;
+	}
+	.classic { padding: 0.8em 1em; }
+	.custom { padding: 0.5em 0.8em 0.8em 2em; }
+	* html a:hover { background: transparent; }
+	.classic {background: #FFFFAA; border: 1px solid #FFAD33; }
+	.critical { background: #FFCCAA; border: 1px solid #FF3334;	}
+	.help { background: #9FDAEE; border: 1px solid #2BB0D7;	}
+	.info { background: #9FDAEE; border: 1px solid #2BB0D7;	}
+	.warning { background: #FFFFAA; border: 1px solid #FFAD33; }
+	</style>
 </head>
 <body>
 	<a href="..">Kembali</a><br>
@@ -15,7 +49,12 @@
 	$result = curl_exec ($ch);
 	curl_close ($ch); 
 	$arr = json_decode($result,true);
-	echo $result;
+	// echo $result;
+	foreach ($arr['data']['results']['questions'] as $q) {
+		echo '<h2>'.$q['question'].'</h2>';
+		echo '<p>'.$q['answer'].'</p>';
+		echo '<h4><a class="tooltip" href="#">Dasar Hukum : '.$q['reference_law'].'<span class="custom warning">'.$q['excerpt_law'].'</span></a></h4>';
+	}
 	?>
 </body>
 </html>
--- laporan_pelanggaran/index.php
+++ laporan_pelanggaran/index.php
@@ -15,7 +15,52 @@
 	$result = curl_exec ($ch);
 	curl_close ($ch); 
 	$arr = json_decode($result,true);
-	echo $result;
+	// echo $result;
+	$d_arr = array ();
+	foreach ($arr['data']['results']['reports'] as $rep) {
+		// echo $rep['id']."|".$rep['judul_laporan']."|".$rep['tanggal_kejadian']."<br>";
+		$a_arr = array(
+			'media' => '',
+			'credit' => '',
+			'caption' => ''
+			);
+		$d_elm = array(
+			'startDate' => substr($rep['tanggal_kejadian'],0,4).','.substr($rep['tanggal_kejadian'],5,2).','.substr($rep['tanggal_kejadian'],8,2),
+			'endDate' => substr($rep['tanggal_kejadian'],0,4).','.substr($rep['tanggal_kejadian'],5,2).','.(substr($rep['tanggal_kejadian'],8,2)+1),
+			'headline' => $rep['judul_laporan'],
+			'asset' => $a_arr,
+			'text' => $rep['keterangan']
+			);
+		array_push($d_arr, $d_elm);
+	}
+	$t_arr = array (
+		'headline' => 'Campaign Finance',
+		'type' => 'default',
+		'text' => 'People say stuff',
+		'date' => $d_arr
+		);
+	$rep_arr = array (
+		'timeline' => $t_arr
+		);
+	// echo json_encode($cf_arr);
+
+	file_put_contents('report.json', json_encode($rep_arr));
+	file_put_contents('report.arr', serialize($rep_arr));
+	// $p = json_decode(file_get_contents('filename.json'));
+	// echo $p;
+	// foreach ($p['data']['results']['reports'] as $rep) {
+	// 	echo $rep['id']."|".$rep['judul_laporan']."|".$rep['tanggal_kejadian']."<br>";
+	// }
 	?>
+	<div id="timeline-embed"></div>
+	<script type="text/javascript">
+	var timeline_config = {
+		width: "100%",
+		height: "100%",
+		source: 'report.json'
+	}
+	</script>
+	<script type="text/javascript" src="../js/storyjs-embed.js"></script>
+
 </body>
 </html>
